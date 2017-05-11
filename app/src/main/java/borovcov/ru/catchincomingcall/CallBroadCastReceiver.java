8ceb8e03a14a82d12d9013ef634c9120d0f46ee5
Vlad Borovtsov <borovcov.vlad@gmail.com>
2017-05-11 16:52:54 +0700

    add permissions, add empty broadcast receiver

---
+++ app/src/main/java/borovcov/ru/catchincomingcall/CallBroadCastReceiver.java
@@ -0,0 +1,17 @@
+package borovcov.ru.catchincomingcall;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by vladborovtsov on 11.05.17.
+ */
+
+public class CallBroadCastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
